56c28aa0eddb9abd05b84ee296e4a8cd3a0e34e3
Parin Shah <shahparin2312@gmail.com>
2023-06-13 17:23:18 +0530

    fixed not found image error

--- src/components/actors/ActorsGrid.jsx
+++ src/components/actors/ActorsGrid.jsx
@@ -1,12 +1,14 @@
 import ActorsCard from './ActorsCard';
 import { FlexGrid } from '../common/FlexGrid';
+import NotFoundImgSrc from '../lib/not-found.png';
+
 const ActorsGrid = ({ actors }) => {
   return (
     <FlexGrid>
       {actors.map(data => (
         <ActorsCard
           key={data.person.id}
-          image={data.person.image ? data.person.image.medium : 'not-found.png'}
+          image={data.person.image ? data.person.image.medium : NotFoundImgSrc}
           name={data.person.name}
           country={data.person.country ? data.person.country.name : null}
           birthday={data.person.birthday}
